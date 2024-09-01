4395a5375de598e6213af78ee2f6b8b08c86fac5
liting <luz.liting@gmail.com>
2024-09-01 15:09:02 +0800

    chore(vue): add devtool

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
   plugins: [
+    VueDevTools(),
     vue(),
     AutoImport({
       imports: ['vue', '@vueuse/core'],
