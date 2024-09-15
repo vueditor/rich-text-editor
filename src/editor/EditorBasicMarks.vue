abc8637f8d6f49c43814c62e7efc575975465507
liting <luz.liting@gmail.com>
2024-09-15 10:18:52 +0800

    feat(mark): support toggle

--- src/editor/EditorBasicMarks.vue
+++ src/editor/EditorBasicMarks.vue
@@ -10,43 +10,43 @@ const marks = [
     label: 'Bold',
     icon: 'i-mdi:format-bold',
     isActivated: () => props.editor.isActive('bold'),
-    toggle: () => props.editor.chain().focus().setBold().run(),
+    toggle: () => props.editor.chain().focus().toggleBold().run(),
   },
   {
     label: 'Code',
     icon: 'i-mdi:code',
     isActivated: () => props.editor.isActive('code'),
-    toggle: () => props.editor.chain().focus().setCode().run(),
+    toggle: () => props.editor.chain().focus().toggleCode().run(),
   },
   {
     label: 'Italic',
     icon: 'i-mdi:format-italic ',
     isActivated: () => props.editor.isActive('italic'),
-    toggle: () => props.editor.chain().focus().setItalic().run(),
+    toggle: () => props.editor.chain().focus().toggleItalic().run(),
   },
   {
     label: 'Strike',
     icon: 'i-mdi:format-strikethrough-variant ',
     isActivated: () => props.editor.isActive('strike'),
-    toggle: () => props.editor.chain().focus().setStrike().run(),
+    toggle: () => props.editor.chain().focus().toggleStrike().run(),
   },
   {
     label: 'Underline',
     icon: 'i-mdi:format-underline ',
     isActivated: () => props.editor.isActive('underline'),
-    toggle: () => props.editor.chain().focus().setUnderline().run(),
+    toggle: () => props.editor.chain().focus().toggleUnderline().run(),
   },
   {
     label: 'Superscript',
     icon: 'i-mdi:format-superscript ',
     isActivated: () => props.editor.isActive('superscript'),
-    toggle: () => props.editor.chain().focus().setSuperscript().run(),
+    toggle: () => props.editor.chain().focus().toggleSubscript().run(),
   },
   {
     label: 'Subscript',
     icon: 'i-mdi:format-subscript ',
     isActivated: () => props.editor.isActive('subscript'),
-    toggle: () => props.editor.chain().focus().setSubscript().run(),
+    toggle: () => props.editor.chain().focus().toggleSubscript().run(),
   },
 ]
 </script>
@@ -57,7 +57,7 @@ const marks = [
       v-for="item in marks"
       :key="item.label"
       class="cursor-pointer rounded p-1 transition-colors hover:bg-zinc-300 dark:hover:bg-zinc-700"
-      :class="{ 'bg-blue-600 text-white': item.isActivated() }"
+      :class="{ '!bg-blue-600 !text-white': item.isActivated() }"
       :title="item.label"
       @click="item.toggle"
     >
